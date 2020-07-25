3523d1df12917054554f96ef0d0d12f33b31ee63
Matt Turner <matturner@gmail.com>
2020-07-26 00:10:31 +0100

    Read from configMap, not local test data.

--- backend/config.ts
+++ backend/config.ts
@@ -2,4 +2,4 @@ const env = Deno.env.toObject();
 
 export const APP_HOST = env.APP_HOST || "0.0.0.0";
 export const APP_PORT = env.APP_PORT || 4000;
-export const DB_PATH = env.DB_PATH || "./data/";
+export const DB_PATH = env.DB_PATH || "/data/";
